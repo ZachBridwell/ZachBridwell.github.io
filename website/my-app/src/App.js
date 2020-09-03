a6e94f2dc65b7798c4d57b38bdc9d1498dbc4e3d
Zach Bridwell <bridwellzac@ymail.com>
2020-09-03 17:51:26 -0400

    website changes

--- website/my-app/src/App.js
+++ website/my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import zach from './Media/temp.jpg'
+import zach from './Media/zach.jpg'
 import resume from './Media/Resume.pdf'
 import Info from './Components/Info.js';
 import Navbar from './Components/Navbar.js';
@@ -36,13 +36,16 @@ function App() {
             <Menu
               size='large'
               pointing
+              fixed = "top"
             >
               <Container>
 
-                <Menu.Item as='a' position='left'  href='/#about'>About Me</Menu.Item>
-                <Menu.Item as='a'position='left' href='/#proj'>Projects</Menu.Item>
-                <Menu.Item as='a'position='left' href='/#research'>Research</Menu.Item>
-                <Menu.Item position='right'>
+                <Menu.Item as='a' position='left'  href='/#about' style={{transform: "translate(-70px, 0px)"}}>About Me</Menu.Item>
+                <Menu.Item as='a'position='left' href='/#proj'  style={{transform: "translate(-90px, 0px)"}}>Projects</Menu.Item>
+                <Menu.Item as='a'position='left' href='/#research'  style={{transform: "translate(-110px, 0px)"}}>Research</Menu.Item>
+                <Menu.Item as='a'position='left' href='/#skills'  style={{transform: "translate(-130px, 0px)"}}>Technical Skills</Menu.Item>
+                <Menu.Item position='right'  style={{transform: "translate(110px, 0px)"}}>
+                  
                   <Button as='a' 
                     href='https://www.linkedin.com/in/zach-bridwell-6367aa170?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BYQyUcyswQESnavfc6%2BslVw%3D%3D'
                     target='_blank'                 
@@ -50,7 +53,7 @@ function App() {
                     LinkedIn
                   </Button>
                 </Menu.Item>
-                <Menu.Item position="right">
+                <Menu.Item position="right"  style={{transform: "translate(90px, 0px)"}}>
                   <Button as='a'
                    href= 'https://github.com/ZachBridwell'
                    target='_blank' 
@@ -75,10 +78,15 @@ function App() {
                 <p>zbridwel@gmail.com</p>
             </Container>
             <Container textAlign='left' >
-              <div class="ui centered big circular image" style={{transform: "translate(250px, -120px)"}}>
+              <div class="ui large centered circular image" style={{transform: "translate(-40px, -120px)"}}>
                 <img src={zach}/>
               </div>
             </Container>
+            <Container style={{transform: "translate(0px, -30px)"}}>
+              <p2> Looking to obtain an entry level Software Engineering position where I can contribute my 
+                acquired knowledge and skills towards organizational growth</p2>
+            </Container>
+            <Button style={{transform: "translate(0px, -10px)"}}as='a' href={resume} target='_blank'>Resume</Button>
           </Segment>
            <Segment style={{ padding: '8em 0em' }} vertical>
            <Container text>
@@ -154,7 +162,77 @@ function App() {
                     </List.Content>
                   </List.Item>
                 </List>
+
+
+                <Divider
+               as='h4'
+               className='header'
+               horizontal
+               style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+             >
+             </Divider>
+
+             <Header as='h3' style={{ fontSize: '2em' }} id={'skills'}>
+               Technical Skills
+             </Header>
+             <Container>
+               <Grid divided inverted stackable>
+               <Grid.Row>
+                <Grid.Column width={5}>
+                <List bulleted>
+                  <List.Item>
+                    <b>Languages</b>
+                    <List.List>
+                      <List.Item>C++ - Advanced</List.Item>
+                      <List.Item>React - Advanced</List.Item>
+                      <List.Item>Python - Intermediate</List.Item>
+                      <List.Item>Java  - Intermediate </List.Item>
+                      <List.Item>Django - Beginner </List.Item>
+                      <List.Item>HTML - Beginner</List.Item>
+                    </List.List>
+                  </List.Item>
+                </List>
+                </Grid.Column>
+
+                <Grid.Column width={5}>
+                <List bulleted>
+                  <List.Item>
+                    <b>Tools</b>
+                    <List.List>
+                      <List.Item>Visual Studio</List.Item>
+                      <List.Item>Git</List.Item>
+                      <List.Item>Postman</List.Item>
+                    </List.List>
+                  </List.Item>
+                </List>
+                </Grid.Column>
+
+
+                <Grid.Column width={5}>
+                <List bulleted>
+                  <List.Item>
+                    <b>Operating Systems</b>
+                    <List.List>
+                      <List.Item>MacOS</List.Item>
+                      <List.Item>Linux</List.Item>
+                      <List.Item>Microsoft Windows</List.Item>
+                    </List.List>
+                  </List.Item>
+                </List>
+                </Grid.Column>
+
+                </Grid.Row>
+                </Grid>
+            </Container> 
+
+
+
+
+
            </Container>
+
+           
+
          </Segment>
     
     
@@ -164,7 +242,7 @@ function App() {
         <Grid divided inverted stackable>
           <Grid.Row>
             <Grid.Column width={15}>
-              <Header inverted as='h4' content='Contact' />
+              <Header inverted as='h4' content='Contact Me' />
               <List link inverted>
                 <List.Item as='a'>zbridwel@gmail.com</List.Item>
                 <List.Item as='a' 
